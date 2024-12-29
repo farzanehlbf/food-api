0c67bae4463c26fc350fc6ad45512cc3af9e52b2
farzaneh labafi <farzaneh.lbf@gmail.com>
2024-12-29 11:51:13 +0330

    create first api project

---
+++ admin_panel/resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>admin panel</title>
+
+   
+       
+    </head>
+    <body>
+      <h1>admin panel|| laravel api eCommerce</h1>
+    </body>
+</html>
---
+++ shop/resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>shop panel</title>
+
+   
+       
+    </head>
+    <body>
+      <h1>shop panel|| laravel api eCommerce</h1>
+    </body>
+</html>
